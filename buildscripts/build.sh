608ce4b8228beac869b8db4bdf86e65fdd95e507
Dan Ware <scandal.uk@gmail.com>
2022-10-03 19:39:42 +0100

    Further build fixes

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -132,6 +132,22 @@ echo "==> Build using $NCPU CPUs"
 mkdir -p build/$ARCH/
 mkdir -p prefix/$ARCH/
 
+cd build
+
+# Build a version of ICU for the host so that it can use the tools during the cross-compilation
+mkdir -p icu-host-build
+cd icu-host-build
+if [ -r ../extern/fetched/icu/icu4c/source/configure ]; then
+    ICU_SOURCE_DIR=../extern/fetched/icu/icu4c/source
+else
+    wget https://github.com/unicode-org/icu/archive/refs/tags/release-70-1.zip
+    unzip release-70-1.zip
+    ICU_SOURCE_DIR=./icu-release-70-1/icu4c/source
+fi
+${ICU_SOURCE_DIR}/configure --disable-tests --disable-samples --disable-icuio --disable-extras CC="ccache gcc" CXX="ccache g++"
+make -j $(nproc)
+cd ../..
+
 # symlink lib64 -> lib so we don't get half the libs in one directory half in another
 mkdir -p prefix/$ARCH/lib
 ln -sf lib prefix/$ARCH/lib64
